db4da2492280cdea090707281b49d6f20d4e08f6
nicky goodman <nickyggoodman@gmail.com>
2024-11-07 11:21:52 -0500

    add default values

--- rentorbuy/src/App.jsx
+++ rentorbuy/src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [inputValues, setInputValues] = useState(
+    {
+      homePrice: 500000
+    }
+  )
 
   return (
     <>
@@ -9,17 +16,17 @@ function App() {
       <h2>Location</h2>
       <div>
         <label htmlFor="desiredLocation">Desired location</label>
-        <input type="text" name="desiredLocation" id="desiredLocation"/>
+        <input defaultValue={inputValues.homePrice} type="number"  name="desiredLocation" id="desiredLocation"/>
       </div>
       <h2>Owning</h2>
       <div>
         <label htmlFor="downPayment">Down payment</label>
-        <input type="text" name="downPayment" id="downPayment" />
+        <input type="number" defaultValue="0" name="downPayment" id="downPayment" />
       </div>  
 
       <div>
         <label htmlFor="mortgageRate">Mortgage rate</label>
-        <input type="text" name="mortgageRate" id="mortgageRate" />
+        <input type="number" defaultValue="0" name="mortgageRate" id="mortgageRate" />
       </div>
 
       <div>
@@ -32,84 +39,84 @@ function App() {
     
       <div>
         <label htmlFor="homeInsurance">Home insurance</label>
-        <input type="text" name="homeInsurance" id="homeInsurance" />
+        <input type="number" defaultValue="0" name="homeInsurance" id="homeInsurance" />
       </div>
 
       <div>
         <label htmlFor="closingCosts">Desired location</label>
-        <input type="text" name="closingCosts" id="closingCosts" />
+        <input type="number" defaultValue="0" name="closingCosts" id="closingCosts" />
       </div>
 
       <div>
         <label htmlFor="HOA/condo fees">HOA/condo fees</label>
-        <input type="text" name="HOA/condo fees" id="HOA/condo fees" />
+        <input type="number" defaultValue="0" name="HOA/condo fees" id="HOA/condo fees" />
       </div>
 
       <div>
         <label htmlFor="monthlyMaintenance">Monthly maintenance</label>
-        <input type="text" name="monthlyMaintenance" id="monthlyMaintenance" />
+        <input type="number" defaultValue="0" name="monthlyMaintenance" id="monthlyMaintenance" />
       </div>
     
       <div>
         <label htmlFor="propertyTax">Property tax</label>
-        <input type="text" name="propertyTax" id="propertyTax"/>
+        <input type="number" defaultValue="0" name="propertyTax" id="propertyTax"/>
       </div>
 
       <div>
         <label htmlFor="homePriceGrowth">Home price growth rate</label>
-        <input type="text" name="homePriceGrowth" id="homePriceGrowth"/>
+        <input type="number" defaultValue="0" name="homePriceGrowth" id="homePriceGrowth"/>
       </div>
     
       <div>
         <label htmlFor="stayDuration">Years planned to stay</label>
-        <input type="text" name="stayDuration" id="stayDuration"/>
+        <input type="number" defaultValue="0" name="stayDuration" id="stayDuration"/>
       </div>
       
       <h2>Renting</h2>
 
       <div>
         <label htmlFor="desiredRent">Desired rent</label>
-        <input type="text" name="desiredRent" id="desiredRent"/>
+        <input type="number" defaultValue="0" name="desiredRent" id="desiredRent"/>
       </div>
 
       <div>
         <label htmlFor="renterInsurance">Rent insurance</label>
-        <input type="text" name="renterInsurance" id="renterInsurance"/>
+        <input type="number" defaultValue="0" name="renterInsurance" id="renterInsurance"/>
       </div>
 
       <div>
         <label htmlFor="securityDeposit">Security deposit</label>
-        <input type="text" name="securityDeposit" id="securityDeposit"/>
+        <input type="number" defaultValue="0" name="securityDeposit" id="securityDeposit"/>
       </div>
 
       <div>
         <label htmlFor="petDeposit">Pet deposit</label>
-        <input type="text" name="petDeposit" id="petDeposit"/>
+        <input type="number" defaultValue="0" name="petDeposit" id="petDeposit"/>
       </div>
 
       <div>
         <label htmlFor="utilIncluded">Utilities included</label>
-        <input type="text" name="utilIncluded" id="utilIncluded"/>
+        <input type="number" defaultValue="0" name="utilIncluded" id="utilIncluded"/>
       </div>
 
       <div>
         <label htmlFor="appFee">Application fee</label>
-        <input type="text" name="appFee" id="appFee"/>
+        <input type="number" defaultValue="0" name="appFee" id="appFee"/>
       </div>
 
       <div>
         <label htmlFor="parkingFee">Parking fee</label>
-        <input type="text" name="parkingFee" id="parkingFee"/>
+        <input type="number" defaultValue="0" name="parkingFee" id="parkingFee"/>
       </div>
 
       <div>
         <label htmlFor="maintenanceFee">Maintenance fee</label>
-        <input type="text" name="maintenanceFee" id="maintenanceFee"/>
+        <input type="number" defaultValue="0" name="maintenanceFee" id="maintenanceFee"/>
       </div>
 
       <div>
         <label htmlFor="amenitiesFee">Amenities fee</label>
-        <input type="text" name="amenitiesFee" id="amenitiesFee"/>
+        <input type="number" defaultValue="0" name="amenitiesFee" id="amenitiesFee"/>
       </div>
 
       <h2>Total costs</h2>
