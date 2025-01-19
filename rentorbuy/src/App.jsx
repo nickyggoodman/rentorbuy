42759b0c9009f58c726e149405aa60e44fad80c0
nicky goodman <nickyggoodman@gmail.com>
2025-01-19 13:39:13 -0500

    add intersection indicatorline

--- rentorbuy/src/App.jsx
+++ rentorbuy/src/App.jsx
@@ -99,7 +99,6 @@ function App() {
 
     // add closing costs
     costArr[costArr.length - 1] += closingCosts;
-    console.log(costArr);    
     return costArr; 
   }
 
@@ -177,14 +176,47 @@ function App() {
     return totalCost.toFixed(2);
   }
 
+  function breakEvenMonth() {
+    let ownerData = genOwnerCostArr();
+    let renterData = genRenterCostArr();
 
+    let i = 0;
+    let intersect = false;
+    while (!intersect && i < 500 ) {
+      if ((ownerData[i] < renterData[i] && ownerData[i + 1] > renterData[i + 1]) ||
+      (ownerData[i] > renterData[i] && ownerData[i + 1] < renterData[i + 1])) {
+        intersect = true;
+      }
+      i++;
+    }
+    console.log("break even month: " + i);
+    return i;
+  }
+
+
+  const lineAtBreakEven = {
+    id: 'lineAtBreakEven',
+    
+    afterDatasetDraw(chart, args, options) {
+      const { ctx, chartArea: {top, bottom}, scales: {x} } = chart;
+      ctx.save();
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'rgba(0, 0, 0, 0.3)';
+      ctx.setLineDash([10, 5]);
+      console.log("intersection at " + options.intersection);
+      ctx.moveTo(x.getPixelForValue(options.intersection), top);
+      ctx.lineTo(x.getPixelForValue(options.intersection), bottom);
+      ctx.stroke();
+      ctx.closePath();
+    }
   
+  } 
 
   return (
     <>
       <CalcForm 
         updateInputValue = {(k, v)=>{
-          console.log("updating " + k + " value")
           setInputValues({
             ...inputValues,
             [k] : v
@@ -194,6 +226,11 @@ function App() {
 
       <Line 
         options = {{
+          plugins: {
+            lineAtBreakEven: {
+              intersection: breakEvenMonth() 
+            }
+          },
           responsive: true,
           elements: {
             point: {
@@ -204,6 +241,7 @@ function App() {
             }
           }
         }}
+        plugins = {[lineAtBreakEven]}
         data ={{
           labels: Array(inputValues.stayDuration*12).fill().map((_, i) => (i+1)),
           datasets: [
@@ -237,4 +275,3 @@ function App() {
 }
 
 export default App
-
